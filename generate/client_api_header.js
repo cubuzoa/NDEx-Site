1bb49f1a07089fabac3bf318c335db99627c9e73
Dexter Pratt <dexterpratt.bio@gmail.com>
2013-09-03 10:48:09 -0700

    Updated ndexClient.js to use basic authentication
    set ndexPassword and ndexUsername in localStorage, otherwise a default
    guest pseudo-account is used

--- generate/client_api_header.js
+++ generate/client_api_header.js
@@ -1,57 +1,107 @@
 /*
 
-ndexClient can be used either as an node.js module or as a client library
+ ndexClient can be used either as an node.js module or as a client library
 
-JQuery is passed to the closure as $.
+ JQuery is passed to the closure as $.
 
-Expected to be a global for the client, expected to be undefined and then required for node.js
+ Expected to be a global for the client, expected to be undefined and then required for node.js
 
+ */
+
+
+(function (exports) {
+
+    if (typeof($) === 'undefined') {
+        console.log("requiring jQuery");
+        $ = require('jQuery');
+    }
+
+    exports.host = "http://localhost:3333";
+
+    exports.guest = {username: 'guest', password: 'guestpassword'}
+
+    function currentCredentials() {
+        if (typeof(localStorage) != 'undefined' && localStorage) {
+            if (localStorage.password, localStorage.username) {
+                return {password: localStorage.ndexPassword, username: localStorage.ndexUsername};
+            } else {
+                return exports.guest;
+            }
+        }
+        return exports.guest;
+
+    }
+
+    function encodedCredentials(){
+        var credentials = currentCredentials();
+        return btoa(credentials.username + ":" + credentials.password);
+    }
+
+
+    exports.defaultNDExErrorHandler = function (data) {
+        console.log("Error : " + JSON.stringify(data));
+        //alert("Error : " + JSON.stringify(data));
+    }
+
+    exports.ndexGet = function (route, queryArgs, callback, errorHandler) {
+
+        $.ajax({
+            type: "GET",
+            /*
+             password: credentials.password,
+             username: credentials.username,
+             xhrFields: {
+             withCredentials: true
+             },
+             */
+            beforeSend: function(xhr){
+                xhr.setRequestHeader("Authorization", "Basic " + encodedCredentials());
+            },
+            url: exports.host + route,
+            data: queryArgs,
+            dataType: "JSON",
+            success: callback,
+            error: errorHandler || exports.defaultNDExErrorHandler
+        });
+    }
+
+    exports.ndexPost = function (route, postData, callback, errorHandler) {
+        $.ajax({
+            type: "POST",
+/*
+            password: credentials.password,
+            username: credentials.username,
+            xhrFields: {
+                withCredentials: true
+            },
 */
+            beforeSend: function(xhr){
+                xhr.setRequestHeader("Authorization", "Basic " + encodedCredentials());
+            },
+            url: exports.host + route,
+            data: postData,
+            dataType: "JSON",
+            success: callback,
+            error: errorHandler || exports.defaultNDExErrorHandler
+        });
+    }
 
-
-
-(function(exports){
-
-	if (typeof($) === 'undefined'){
-		console.log("requiring jQuery");
-		$ = require('jQuery');
-	}
-	
-	exports.host = "http://localhost:3333";
-	
-	exports.defaultNDExErrorHandler = function(data){
-		console.log("Error : " + JSON.stringify(data));
-		//alert("Error : " + JSON.stringify(data));
-	}
-
-	exports.ndexGet = function (route, queryArgs, callback, errorHandler){
-		$.ajax({
-			   type: "GET",
-			   url: exports.host + route,
-			   data: queryArgs,
-			   dataType: "JSON",
-			   success: callback,
-			   error: errorHandler || exports.defaultNDExErrorHandler
-		});
-	}
-
-	exports.ndexPost = function(route, postData, callback, errorHandler){
-		$.ajax({
-			   type: "POST",
-			   url: exports.host + route,
-			   data: postData,
-			   dataType: "JSON",
-			   success: callback,
-			   error: errorHandler || exports.defaultNDExErrorHandler
-		});
-	}
-
-	exports.ndexDelete = function(route, callback, errorHandler){
-		$.ajax({
-			   type: "DELETE",
-			   url: exports.host + route,
-			   success: callback,
-			   error: errorHandler || exports.defaultNDExErrorHandler
-		});
-	}
+    exports.ndexDelete = function (route, callback, errorHandler) {
+        $.ajax({
+            type: "DELETE",
+            /*
+             password: credentials.password,
+             username: credentials.username,
+             xhrFields: {
+             withCredentials: true
+             },
+             */
+            beforeSend: function(xhr){
+                xhr.setRequestHeader("Authorization", "Basic " + encodedCredentials());
+            },
+            url: exports.host + route,
+            success: callback,
+            error: errorHandler || exports.defaultNDExErrorHandler
+        });
+    }
 
